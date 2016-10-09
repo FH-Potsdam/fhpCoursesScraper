8ab58fe3bc4539112f5aa49b7f0b7b115cc146f8
Joseph Ribbe <social@coderwelsch.com>
2016-10-09 14:50:06 +0200

    Added max attendances regex & parsing

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ let rawDataPrepare = new RawDataPrepare(),
 			moduleName: "14W2D-DP",
 			title: "Gestaltungsgrundlagen Atelier Farbe:",
 			tutors: "Jens Tix, Marcus Große, Prof. Klaus Keller",
-			description: "Lorem Ipsum dolor sit amet. Workspace: https://incom.org/workspace/6886. Fabian Morón Zirfas, Anderer Link: https://www.iri-thesys.org/research/research-groups/Innovative-sustainable-land-management/ginkoo/ginkoo-project",
+			description: "Lorem Ipsum dolor sit amet. Workspace: https://incom.org/workspace/6886. Fabian Morón Zirfas. Die Teilnehmerzahl ist auf 20 Teilnehmer_innen begrenzt. Anderer Link: https://www.iri-thesys.org/research/research-groups/Innovative-sustainable-land-management/ginkoo/ginkoo-project",
 			time: "Freitag 10.00 – 16.00 Uhr, und Donnerstag 10.00, Beginn: 24.10.2016",
 			location: "LW 122 Foto S/W-Labor"
 		}, {
